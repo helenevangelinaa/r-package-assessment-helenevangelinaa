3fa6a623cb3edfcff24ec155b73a96732a058416
Helen Evangelina <heva0001@student.monash.edu>
2020-10-20 21:43:11 +1100

    Fixing non ASCII characters

--- data-raw/worldshp.R
+++ data-raw/worldshp.R
@@ -1,3 +1,5 @@
-worldshp <- rgdal::readOGR(dsn = file.path(paste(getwd(),"/extdata", sep=""), "TM_WORLD_BORDERS_SIMPL-0.3.shp") , stringsAsFactors = F)
+worldshp <- rgdal::readOGR(dsn = file.path(paste(getwd(),"/inst/extdata", sep=""), "TM_WORLD_BORDERS_SIMPL-0.3.shp") , stringsAsFactors = F)
+
+worldshp$NAME <- gsub("[^\x20-\x7E]", "", worldshp$NAME)
 
 usethis::use_data(worldshp, overwrite = TRUE)
